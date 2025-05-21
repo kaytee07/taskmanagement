0343c726474412b0edf930349ca9da82f1d73573
kaytee07 <kofi.taylor07@gmail.com>
2025-05-21 13:44:31 +0000

    feat: add TaskStatus to map to the statuses table in the database

---
+++ src/main/java/com/project/taskmanagement/entity/TaskStatus.java
@@ -0,0 +1,45 @@
+package com.project.taskmanagement.entity;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "statuses")
+public class TaskStatus {
+
+    @Id
+    @Column(name = "status_id", nullable = false, length = 36)
+    private String statusId;
+
+    @Column(name = "name", nullable = false, unique = true, length = 60)
+    private String name;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+
+
+    public String getStatusId() {
+        return statusId;
+    }
+
+    public void setStatusId(String statusId) {
+        this.statusId = statusId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
